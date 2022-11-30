673459a0baf99cb883ca65723423e0eb15f9214d
Matia <hoar-frost1@yandex.ru>
2022-11-30 15:12:46 +0300

    ДЗ№2. Доработка 30.11.2022

--- task_2/solution.js
+++ task_2/solution.js
@@ -8,7 +8,6 @@ function calcShipping(sum, min, shipping) {
     // создайте переменную shippingSum
     if(productsSum == 0 && productsSum >= freeShippingMinSum){
         shippingSum = 0;
-      } 
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
     // если productsSum Больше или равна freeShippingMinSum,
@@ -17,6 +16,7 @@ function calcShipping(sum, min, shipping) {
     // то shippingSum присвоить значение shippingPrice
         }else {
            shippingSum = shippingPrice;
+        }
     // Конец решения задания №2.1.
 
     return shippingSum;
