8e55a8e26e3cbaddebf9087b6ee7f481a2fcb130
Matia <hoar-frost1@yandex.ru>
2022-11-30 11:29:56 +0300

    Задание 2. Доработка 30.11.2022

--- task_2/solution.js
+++ task_2/solution.js
@@ -6,23 +6,17 @@ function calcShipping(sum, min, shipping) {
     // Задание №2.1. Рассчитать доставку
     let shippingSum;
     // создайте переменную shippingSum
-    if(productsSum = 0){
+    if (productsSum == 0 && productsSum >= freeShippingMinSum) {
         shippingSum = 0;
-        }else {
+      } 
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
-            if(productsSum >= freeShippingMinSum){
-                shippingSum = 0;
-            }else {
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
-                if(productsSum > 0 AND productsSum < freeShippingMinSum) {
-                    shippingSum = shippingPrice;
-                }
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-        }
-    }
+        }else {
+            shippingSum = shippingPrice;
     // Конец решения задания №2.1.
 
     return shippingSum;
@@ -36,15 +30,10 @@ function calcDiscount(sum, min, discount) {
     // Задание №2.2. Рассчитать скидку
     let = discountSum; 
     // создайте переменную discountSum
-    if(productsSum >= discountMinSum) {
-        discountSum = discountPart * productsSum / 100;
-        }else if (productsSum <= discountMinSum) {
-            discountSum = 0;
-        }
+    let discountSum = productsSum >= discountMinSum ? discountPart * productsSum / 100 : 0;
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
-
     // Конец решения задания №2.2.
 
     return discountSum;
