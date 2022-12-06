ea191e7ff660692c8e1bad8f5dc1c4c20f83a8fa
Matia <hoar-frost1@yandex.ru>
2022-12-06 10:03:39 +0300

    ДЗ 3. Доработка 06.12.22

--- task_3/solution.js
+++ task_3/solution.js
@@ -1,7 +1,6 @@
 function renderCartItem(item) {
     const product = item;
     // Задание №3.1. Формирование строки корзины
-    let product = {id: 1, title: "Пицца", count: 5, price: "500.00"};
     // product - объект вида {id: id, title: title, count: count, price: price}
     // например, {id: 1, title: 'Пицца', count: 5, price: '500.00'}, где
     // id - идентификатор
